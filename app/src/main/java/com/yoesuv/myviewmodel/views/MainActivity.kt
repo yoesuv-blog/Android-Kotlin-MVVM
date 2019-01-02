870ad50647c07cfe6cfbbe3b30e8dc9ea2542df6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-01-02 11:16:18 +0700

    :pencil: set viewmodel to view

--- app/src/main/java/com/yoesuv/myviewmodel/views/MainActivity.kt
+++ app/src/main/java/com/yoesuv/myviewmodel/views/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.yoesuv.myviewmodel.views
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.yoesuv.myviewmodel.R
+import com.yoesuv.myviewmodel.databinding.ActivityMainBinding
+import com.yoesuv.myviewmodel.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.main = viewModel
     }
 }
