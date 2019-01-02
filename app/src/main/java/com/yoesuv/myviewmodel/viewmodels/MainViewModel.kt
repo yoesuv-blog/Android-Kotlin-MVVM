858c8d2b78b43eeba5eb4104239274dcd1d30bbc
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-01-02 11:05:22 +0700

    :pencil: add class viewmodel

---
+++ app/src/main/java/com/yoesuv/myviewmodel/viewmodels/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.yoesuv.myviewmodel.viewmodels
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+}
--- app/src/main/java/com/yoesuv/myviewmodel/views/MainActivity.kt
+++ app/src/main/java/com/yoesuv/myviewmodel/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yoesuv.myviewmodel
+package com.yoesuv.myviewmodel.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yoesuv.myviewmodel.R
 
 class MainActivity : AppCompatActivity() {
 
